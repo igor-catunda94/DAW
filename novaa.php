443fd97a113d34fccfbcb77e72c73086fad02cfb
igor-catunda94 <69877180+igor-catunda94@users.noreply.github.com>
2020-08-18 20:10:12 -0300

    Add files via upload
    Página com um echo PHP.

---
+++ novaa.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Uma página que voce bota os dados</title>
+    <link rel="stylesheet" href="./main.css">
+
+</head>
+<body>
+    <div id="meio"><?php 
+    	$string = "Olá, mundo. Tudo bem? Sou um echo de PHP :)";
+    	echo $string;
+    	
+    	?>
+    	</div>
+</body>
+</html>
